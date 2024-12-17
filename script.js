25e94eafd824d1d3dacd2e14fb16a2e72a48250c
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2024-12-17 18:24:52 -0300

    Add logic to play 5 rounds

--- script.js
+++ script.js
@@ -34,3 +34,69 @@ function getHumanChoice() {
         }
     } while(wrongInput)
 }
+
+function playRound(humanChoice, computerChoice) {
+    // Case: Draw, both choices are equal
+    if (humanChoice === computerChoice) {
+        console.log("Draw! Both chose " + humanChoice);
+        return;
+    }
+
+    // Case human chose Rock
+    if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            console.log("You lose! Paper beats Rock");
+            computerScore++;
+            return;
+        }
+        
+        if (computerChoice === "scissors") {
+            console.log("You win! Scissors beats Rock");
+            humanScore++;
+            return;
+        }
+    }
+
+    // Case human chose Paper
+    if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("You win! Paper beats Rock");
+            humanScore++;
+            return;
+        }
+        
+        if (computerChoice === "scissors") {
+            console.log("You lose! Scissors beats Paper");
+            computerScore++;
+            return;
+        }
+    }
+
+    // Case human chose scissors
+    if (humanChoice === "scissors") {
+        if (computerChoice === "paper") {
+            console.log("You win! Scissors beats Paper");
+            humanScore++;
+            return;
+        }
+        
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock beats Scissors");
+            computerScore++;
+            return;
+        }
+    }
+}
+
+for (let i = 1; i < 6; i++) {
+    console.log(`Round ${i}/5`);
+    playRound(getHumanChoice(), getComputerChoice());
+}
+
+if (humanScore === computerScore) {
+    console.log("Whoa, a Draw!");
+} else if (humanScore > computerScore) {
+    console.log("Congratulations, you win!");
+} else {
+    console.log("Computer wins, try again!");
+}
